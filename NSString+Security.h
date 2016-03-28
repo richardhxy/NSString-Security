2c78f19419bace257b47cd40c0044878f6a6e209
richardhxy <huangxingya@gmail.com>
2016-03-28 20:52:40 +0800

    Update NSString+Security.h

--- NSString+Security.h
+++ NSString+Security.h
@@ -1,1 +1,26 @@
+#import <Foundation/Foundation.h>
 
+@interface NSString (Base64Addition)
++(NSString *)stringFromBase64String:(NSString *)base64String;
++(NSString *)stringFromBase64UrlEncodedString:(NSString *)base64UrlEncodedString;
+-(NSString *)base64String;
+-(NSString *)base64UrlEncodedString;
+@end
+
+@interface NSData (Base64Addition)
++(NSData *)dataWithBase64String:(NSString *)base64String;
++(NSData *)dataWithBase64UrlEncodedString:(NSString *)base64UrlEncodedString;
+-(NSString *)base64String;
+-(NSString *)base64UrlEncodedString;
+@end
+
+NSString *NSStringNotNull(NSString *string);
+
+@interface NSString (Hashes)
+
+- (NSString *)md5;
+- (NSString *)sha1;
+- (NSString *)sha256;
+- (NSString *)sha512;
+
+@end
